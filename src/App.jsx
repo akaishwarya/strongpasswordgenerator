013776b7db62d0312b529a3ad0acf5e535b97f3f
Aishwarya <akaishwarya88@gmail.com>
2024-12-17 11:13:18 +0530

    strongpasswordgenerator react to github

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { PasswordGenerator } from "./passwordGenerator.jsx"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+       <PasswordGenerator />
+    </>
+  )
+}
+
+export default App
